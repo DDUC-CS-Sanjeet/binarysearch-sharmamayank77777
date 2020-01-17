a9d48f375d17ea700f4a2c52a21864531162b2e2
MayankSharma <sharmamayank77777@gmail.com>
2020-01-17 17:03:38 +0530

    Completed

--- BinSearch.cpp
+++ BinSearch.cpp
@@ -1,20 +1,63 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 bool binarySearchReacursive(int* array, int startIndex, int lastIndex, int element)
 {
-  return true;
+	int mid;
+	while(startIndex<=lastIndex)
+	{
+		mid=(startIndex+lastIndex)/2;
+		if(array[mid]==element)
+		return true;
+		else
+		if(array[mid]<element)
+	    return	binarySearchReacursive(array,mid+1,lastIndex,element);
+		else
+		return	binarySearchReacursive(array,startIndex,mid-1,element);
+	}
+  return false;
 }
 
 bool binarySearchIterative(int* array, int startIndex, int lastIndex, int element)
 {
-  return true;
+	int mid;
+	while(startIndex<=lastIndex)
+	{
+		if(array[mid]==element)
+		return true;
+		else if(array[mid]<element)
+		startIndex=mid-1;
+		else
+		lastIndex=mid+1;
+		}	
+  return false;
 }
 
 int main()
 {
-
-
+	int size;
+	cout<<"Enter size of array:";
+	cin>>size;
+	int *array=new int[size],i,num;
+	cout<<"\nEnter elements of array:";
+	for(i=0;i<size;i++)
+	cin>>array[i];
+	cout<<"\nEnter element to be search:";
+	cin>>num;
+	sort(array,array+size);
+	bool result1=binarySearchReacursive(array,0,size-1,num);
+	bool result2=binarySearchIterative(array,0,size-1,num);
+	cout<<"************* Recursion Result***********";
+	if(result1==false)
+	cout<<"\nGiven number is not present in array";
+	else
+	cout<<"\nGiven number is present in array";
+    cout<<"\n************* Iterative Result***********";
+    if(result2==false)
+	cout<<"\nGiven number is not present in array";
+	else
+	cout<<"\nGiven number is present in array";
 
   return 0;
 }
